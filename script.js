9bfea29f27d66379b2dd15b8fdc0a806fe705776
Adam <adamlenter1@gmail.com>
2021-09-22 15:22:09 -0400

    Set up so that a basic professional exprience heading pops up when you scroll over that heading.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const profilePictureDiv = document.getElementById("profilePictureDiv");
+const professionalExperienceDiv = document.getElementById("professionalExperienceDiv");
+const professionalExperienceLI = document.getElementById("professionalExperienceLI");
+
+function displaySelectedDiv(selectedDiv)
+    {
+    const divsToHide = ["profilePictureDiv", "professionalExperienceDiv"];
+
+    for(div of divsToHide) {
+        if(div === selectedDiv) {
+            //This is the div to display:
+            document.getElementById(div).style.display = "block";
+        }
+        else {
+            //This is not the div to display:
+            document.getElementById(div).style.display = "none";
+        }
+    }
+}
